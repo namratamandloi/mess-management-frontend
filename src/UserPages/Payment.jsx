18bef811499b1d3928b6cfc33916f5b1b5315efb
Namrata Mandloi <namratamandloi2211@gmail.com>
2023-05-14 11:29:12 +0530

    some changes added

--- src/UserPages/Payment.jsx
+++ src/UserPages/Payment.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import scannerImage from "../assets/scanner.jpeg";
+import scannerImage from "../assets/Namrata_Payment.jpeg";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Navigate } from "react-router-dom";
 
